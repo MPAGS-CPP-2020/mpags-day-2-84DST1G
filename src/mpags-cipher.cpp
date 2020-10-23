18117c2aaf35e0492af38cd0e0baea0c65d1e407
root <root@MSI.localdomain>
2020-10-23 10:24:07 +0100

    Added Translate Char Function

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -6,6 +6,52 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
+std::string translateChar(char inputChar)
+{
+  std::string inputText = "";
+
+  if (std::isalpha(inputChar)) {
+      inputText += std::toupper(inputChar);
+      return inputText;
+    }
+
+    // Transliterate digits to English words
+    switch (inputChar) {
+      case '0':
+	inputText += "ZERO";
+	break;
+      case '1':
+	inputText += "ONE";
+	break;
+      case '2':
+	inputText += "TWO";
+	break;
+      case '3':
+	inputText += "THREE";
+	break;
+      case '4':
+	inputText += "FOUR";
+	break;
+      case '5':
+	inputText += "FIVE";
+	break;
+      case '6':
+	inputText += "SIX";
+	break;
+      case '7':
+	inputText += "SEVEN";
+	break;
+      case '8':
+	inputText += "EIGHT";
+	break;
+      case '9':
+	inputText += "NINE";
+	break;
+    }
+
+    return inputText;
+}
+
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
 {
@@ -111,44 +157,7 @@ int main(int argc, char* argv[])
   while(std::cin >> inputChar)
   {
     // Uppercase alphabetic characters
-    if (std::isalpha(inputChar)) {
-      inputText += std::toupper(inputChar);
-      continue;
-    }
-
-    // Transliterate digits to English words
-    switch (inputChar) {
-      case '0':
-	inputText += "ZERO";
-	break;
-      case '1':
-	inputText += "ONE";
-	break;
-      case '2':
-	inputText += "TWO";
-	break;
-      case '3':
-	inputText += "THREE";
-	break;
-      case '4':
-	inputText += "FOUR";
-	break;
-      case '5':
-	inputText += "FIVE";
-	break;
-      case '6':
-	inputText += "SIX";
-	break;
-      case '7':
-	inputText += "SEVEN";
-	break;
-      case '8':
-	inputText += "EIGHT";
-	break;
-      case '9':
-	inputText += "NINE";
-	break;
-    }
+    inputText += translateChar(inputChar)
 
     // If the character isn't alphabetic or numeric, DONT add it.
     // Our ciphers can only operate on alphabetic characters.
